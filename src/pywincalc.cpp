f4ced30651c71f75168d5e828161ae0bb2c6f2e5
StephenCzarnecki <sczarnecki@lbl.gov>
2019-01-28 17:38:07 -0500

    initial build attempt

---
+++ src/pywincalc.cpp
@@ -0,0 +1,19 @@
+#include <pybind11/pybind11.h>
+
+#include <wincalc/calc_from_disk.h>
+
+namespace py = pybind11;
+
+using namespace pybind11::literals;
+
+PYBIND11_MODULE(pywincalc, m)
+{
+	m.doc() = "Python bindings for WinCalc";
+	
+	py::class_<Gap_Data>(m, "Gap_Data")
+		.def_readwrite("name", &Gap_Data::name)
+		.def_readwrite("thickness", Gap_Data::thickness);
+	
+	m.def("calc_u", &calc_u, "Calculate U-value");
+	m.def("calc_shgc", &calc_shgc, "Calculate SHGC");
+}
