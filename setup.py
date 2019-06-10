8f20de91962441bfb411d95b3db20a24a18f963e
StephenCzarnecki <sczarnecki@lbl.gov>
2019-06-10 15:00:08 -0400

    Incrementing version in setup.py to 1.0 to match best build for public disclosure.  Should have been done in master but doing it retroactivly in a branch instead.  If it becomes an issue we can edit rewrite the actual history.

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pywincalc',
-    version='0.0.2',
+    version='1.0.0',
     ext_modules=[CMakeExtension('pywincalc')],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
