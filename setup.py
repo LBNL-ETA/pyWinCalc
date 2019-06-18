3e3b7d1c953384236b33ae59a4995e9706ffb20c
StephenCzarnecki <sczarnecki@lbl.gov>
2019-06-18 17:04:03 -0400

    v1.0.5 Updating to latest version of WinCalc to get calculation bug fixes.

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pywincalc',
-    version='1.0.4',
+    version='1.0.5',
     ext_modules=[CMakeExtension('pywincalc')],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
