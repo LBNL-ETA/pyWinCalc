ffefe31afc0fa3f895eb4b14970232014c95729c
StephenCzarnecki <sczarnecki@lbl.gov>
2020-05-05 23:42:12 -0400

    Added bindings for NFRC U and SHGC environments.

--- src/pywincalc.cpp
+++ src/pywincalc.cpp
@@ -486,6 +486,9 @@ PYBIND11_MODULE(pywincalc, m) {
 	  .value("ISO_9050", wincalc::Spectal_Data_Wavelength_Range_Method::ISO_9050)
 	  .value("Condensed", wincalc::Spectal_Data_Wavelength_Range_Method::CONDENSED);
 
+  m.def("nfrc_u_environments", &wincalc::nfrc_u_environments);
+  m.def("nfrc_shgc_environments", &wincalc::nfrc_shgc_environments);
+
   py::class_<wincalc::Glazing_System>(m, "Glazing_System")
       .def(py::init<std::vector<wincalc::Product_Data_Optical_Thermal> const &,
                     std::vector<wincalc::Engine_Gap_Info> const &,
