2e2b160ece717ddf9bbb5d6a28fffa780ab12027
StephenCzarnecki <sczarnecki@gmail.com>
2020-05-14 16:16:05 -0400

    Added minimum example file.  Removed api tokens from other example files.

--- example/igsdb_custom_venetian.py
+++ example/igsdb_custom_venetian.py
@@ -22,8 +22,8 @@ bsdf_hemisphere = pywincalc.BSDF_Hemisphere.create(pywincalc.BSDF_Basis.Quarter)
 # Download some product data from the IGSDB.  This example gets a generic single clear 3mm glazing (NFRC 102),
 # and a material to use as part of the woven shade.
 # For more information on getting data from the igsdb please see igsdb.lbl.gov/openapi
-igsdb_api_token = "8ae3205081608099fe0261178ea571ecc3b01fc2"
-url_single_product = "http://localhost:8000/api/v1/products/{id}"  # Template URL for single product
+igsdb_api_token = "INSERT_YOUR_TOKEN_HERE"
+url_single_product = "https://igsdb-development.herokuapp.com/api/v1/products/{id}"  # Template URL for single product
 
 headers = {"Authorization": "Token {token}".format(token=igsdb_api_token)}  # Token authorization headers
 
