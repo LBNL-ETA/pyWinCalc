8fb975c4bb094a91fe93a540b68d8c2a88e602e6
Daniel McQuillen <daniel@mcquilleninteractive.com>
2021-07-23 16:50:00 +1000

    bump version

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pywincalc',
-    version='2.2.2',
+    version='2.2.3',
     long_description='',
     # tell setuptools to look for any packages under 'src'
     packages=find_packages(where='src'),
