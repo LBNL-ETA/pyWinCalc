4878e7f07f511a856eb1f8f340173f5d098236c5
StephenCzarnecki <sczarnecki@lbl.gov>
2022-03-09 00:27:25 -0500

    Update setup.py
    Fixing mistake where generator flag was in build_args.  Should be in cmake_args.

--- setup.py
+++ setup.py
@@ -41,12 +41,12 @@ class CMakeBuild(build_ext):
 
         if platform.system() == "Windows":
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            cmake_args += ['-G', 'Visual Studio 15 2017']
             if sys.maxsize > 2**32:
                 cmake_args += ['-A', 'x64']
             else:
                 cmake_args += ['-A', 'Win32']
             build_args += ['--', '/m']
-            build_args += ['-G', 'Visual Studio 15 2017']
         else:
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
             build_args += ['--', '-j2']
