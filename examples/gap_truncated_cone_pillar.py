53f639aefa04246695c967500f5c5db844efcd25
Simon Vidanovic <dvvidanovic@lbl.gov>
2023-12-08 13:01:08 -0800

    Several pillar examples added.

---
+++ examples/gap_truncated_cone_pillar.py
@@ -0,0 +1,22 @@
+import pywincalc
+
+pillar = pywincalc.TruncatedConePillar(height=0.002, material_conductivity=20,
+                                       cell_area=pywincalc.pillar_cell_area(pywincalc.CellSpacingType.SQUARE, 0.03),
+                                       radius_1=0.25e-3, radius_2=0.01e-3)
+gap = pywincalc.Layers.create_pillar(pillar=pillar, pressure=0.1333)
+
+gaps = [gap]
+
+clear_3_path = "products/CLEAR_3.DAT"
+clear_3 = pywincalc.parse_optics_file(clear_3_path)
+solid_layers = [clear_3, clear_3]
+
+# Create a glazing system.  This only shows an example of getting one result from a glazing system
+# created using default environmental conditions.
+#
+# For more possible results see optical_results_NFRC.py
+#
+# For more on environmental conditions see environmental_conditions_user_defined.py
+glazing_system = pywincalc.GlazingSystem(solid_layers=solid_layers, gap_layers=gaps)
+u_value = glazing_system.u()
+print(f"U-value for the system: {u_value}")
